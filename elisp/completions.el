4bcc2fcb518046ac46b5f5ddcafac7dce72ff534
nonk123 <me@nonk.dev>
2024-06-08 03:49:44 +0300

    Fix completions once and for all

--- elisp/completions.el
+++ elisp/completions.el
@@ -4,7 +4,7 @@
 (require 'embark)
 (require 'cape)
 
-(setq completion-styles '(partial-completion basic orderless))
+(setq completion-styles '(flex partial-completion orderless))
 (setq completion-category-defaults nil)
 
 (setq ispell-alternate-dictionary "/usr/share/dict/words")
@@ -12,12 +12,7 @@
 (setq company-backends nil)
 (setq company-frontends nil)
 
-;; Avoiding `company' at all cost and using it just for the backends.
-(setq-default completion-at-point-functions
-	      (append
-	       (mapcar (lambda (x) (cape-capf-nonexclusive (cape-company-to-capf x)))
-		       (list #'company-files #'company-keywords))
-	       (list (cape-company-to-capf #'company-dabbrev))))
+(setq-default completion-at-point-functions (list #'cape-file #'cape-keyword))
 
 (defun nonk/wrap-elisp-capf (orig)
   (cape-wrap-nonexclusive orig))
@@ -37,6 +32,10 @@
 	   :map company-mode-map
 	   ("C-'" . nonk/try-yas-then-cap))
 
+(bind-keys :map company-active-map
+	   ("C-p" . nil)
+	   ("C-n" . nil))
+
 (setq completion-in-region-function #'consult-completion-in-region)
 
 (setq prefix-help-command #'embark-prefix-help-command)
@@ -47,5 +46,4 @@
 	   ("C-y" . consult-yank-from-kill-ring)
 	   ("M-y" . yank)
 	   ([remap switch-to-buffer] . consult-buffer)
-	   ("C-." . embark-act)
 	   ("M-." . xref-find-definitions))
