f8da5822fedce03f60fad4bc6f9a5a4470dbfd3e
nonk123 <me@nonk.dev>
2024-06-26 08:46:20 +0300

    Allow ignoring `nonk/format-buffer` hook for certain modes

--- elisp/config.el
+++ elisp/config.el
@@ -1,5 +1,6 @@
 (defvar nonk/aggressive-indent-modes '(lisp-data-mode))
 (defvar nonk/ignore-lsp-modes '(sh-mode ld-script-mode lisp-data-mode))
+(defvar nonk/ignore-autoformat-modes '(html-mode))
 (defvar nonk/mode-extras nil)
 
 (setq wdired-allow-to-change-permissions t)
