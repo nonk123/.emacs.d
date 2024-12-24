1ac12a7ed0d52d064988029d6436095c2b46d1ac
nonk123 <me@nonk.dev>
2024-12-24 08:29:16 +0300

    Add `nonk/disable-mode-globally` utility

--- elisp/keys.el
+++ elisp/keys.el
@@ -8,6 +8,24 @@
   (interactive "Fsudo find file: ")
   (find-file (concat "/sudo::" filename)))
 
+(defun nonk/read-minor-mode (&optional enabled-only)
+  "Read a minor-mode from the minibuffer.
+
+If ENABLED-ONLY is not nil, display only minor-modes enabled in the current buffer."
+  (interactive "i")
+  (let ((x (completing-read
+	    "Minor mode: "
+	    (mapcar #'car minor-mode-alist)
+	    (and enabled-only #'symbol-value))))
+    (and x (intern x))))
+
+(defun nonk/disable-mode-globally (mode)
+  "Disable minor MODE in all buffers."
+  (interactive (list (nonk/read-minor-mode)))
+  (dolist (buffer (buffer-list))
+    (with-current-buffer buffer
+      (funcall mode -1))))
+
 (bind-keys ("C-x M-f" . nonk/find-file-sudo))
 
 (bind-keys ("M-*" . replace-regexp)
