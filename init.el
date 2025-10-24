311db26db35c6f6a96990b4964073f5ad2635cbe
nonk123 <me@nonk.dev>
2025-10-24 08:55:10 +0300

    Let's start over (again)

--- init.el
+++ init.el
@@ -1,27 +1,23 @@
 ;; -*- lexical-binding: t; -*-
 
-(defconst nonk/windows? (and (string-match-p "AppData\\\\Roaming" (getenv "HOME")) t))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(defconst nonk/home
-  (if nonk/windows?
-      (concat "C:/Users/" (user-login-name) "/")
-    (expand-file-name "~/"))
-  "Should be used instead of `~' in filenames for portability.")
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
-(defconst nonk/module-order
-  '("init" "straight" "config" "projects" "linting" "completions"
-    "visual" "treemacs" "coding" "keys" "modes" "finalize")
-  "The order in which init-file modules are loaded.  See `nonk/load-init'.")
-
-(require 'bytecomp)
-(require 'comp)
-
-(setq native-comp-async-report-warnings-errors 'silent)
-
-(defun nonk/load-init ()
-  "Load the init-file in parts called modules."
-  (interactive "P")
-  (dolist (module nonk/module-order)
-    (load-file (expand-file-name (concat "elisp/" module ".el") user-emacs-directory))))
-
-(nonk/load-init)
+(use-package magit
+  :bind ("C-x g" . magit))
