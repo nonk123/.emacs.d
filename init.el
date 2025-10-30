a31c3f7a9ddc6b7458b1808a3a9c6046888afed1
nonk123 <me@nonk.dev>
2025-10-30 18:28:30 +0300

    Switch to Projectile's `alien` indexing

--- init.el
+++ init.el
@@ -88,6 +88,8 @@ do that breaks a lot of external packages.")
   :custom
   (projectile-project-search-path `((,(expand-file-name "Sources" nonk/home) . 1)
                                     ("~" . 1)))
+  (projectile-enable-caching t)
+  (projectile-indexing-method 'alien)
   (projectile-auto-cleanup-known-projects t)
   (projectile-mode 1)
   :bind-keymap ("C-c p" . projectile-command-map))
